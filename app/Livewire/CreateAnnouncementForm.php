a7d074d5acc31cf5402e1890c8f4242f4d3cb952
Lorenzo <Lorenzomem56@gmail.com>
2026-08-05 16:04:33 +0200

    8

--- app/Livewire/CreateAnnouncementForm.php
+++ app/Livewire/CreateAnnouncementForm.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Jobs\GoogleVisionSafeSearch;
 use App\Jobs\GoogleVisionLabelImage;
+use App\Jobs\RemoveFaces;
 use Livewire\Component;
 use App\Models\Category;
 use App\Models\Announcement;
@@ -90,9 +91,11 @@ class CreateAnnouncementForm extends Component
                 ]);
 
                 // Invia il Job alla coda passando il path salvato e le dimensioni del crop
-                dispatch(new ResizeImage($newImage->path, 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+            RemoveFaces::withChain([
+                new ResizeImage($newImage->path, 300, 300),
+                new GoogleVisionSafeSearch($newImage->id),
+                new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
 
             File::deleteDirectory(storage_path('app/livewire-tmp'));
